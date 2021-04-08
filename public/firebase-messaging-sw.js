d04a58ba5a24a6574c94a9e0013672b310011a9f
Minh Le Bui Hoang <minhcab.dev.19345@gmail.com>
2021-04-08 18:44:13 +0700

    firebase script changed

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,5 @@
-// importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js')
-// importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js')
-
-// firebase.initializeApp({
-//   messagingSenderId: '825916469639'
-// })
-
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register(".firebase-messaging-sw.js")
-//     .then(function (registration) {
-//       console.log("Registration successful, scope is:", registration.scope);
-//     })
-//     .catch(function (err) {
-//       console.log("Service worker registration failed, error:", err);
-//     });
-// }
-
-// firebase.messaging()
-
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
