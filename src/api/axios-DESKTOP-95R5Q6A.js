7cae70c5ea64f4d6019d1ec71ed3611023effb7d
Minh Le Bui Hoang <minhcab.dev.19345@gmail.com>
2021-04-29 22:52:07 +0700

    Delete unneccessary classes 3

--- src/api/axios-DESKTOP-95R5Q6A.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL: 'http://45.76.161.254:8082/ihelp/api/',
-})
-
-instance.defaults.headers.post['Content-Type'] = 'application/json'
-
-export default instance
